91a59c47c77b26cbdccf77657b804ccbe9080769
Justunee <lifeistogive@gmail.com>
2019-04-20 21:42:25 +0700

    home screen

--- src/view/Home/index.jsx
+++ src/view/Home/index.jsx
@@ -3,9 +3,10 @@ import withStyles from "@material-ui/core/styles/withStyles";
 import homeStyle from "../../assets/jss/homeStyle";
 import Dropzone from "react-dropzone";
 import { Grid } from "@material-ui/core";
+import styled, { ThemeProvider } from "styled-components";
+import NoSsr from "@material-ui/core/NoSsr";
+import { createMuiTheme } from "@material-ui/core/styles";
 import { palette, spacing, typography } from "@material-ui/system";
-import styled from "styled-components";
-import { posix } from "path";
 
 class Home extends Component {
   constructor(props) {
@@ -40,14 +41,40 @@ class Home extends Component {
 
   render() {
     const Box = styled.div`${palette}${spacing}${typography}`;
+    const theme = createMuiTheme({
+      typography: {
+        useNextVariants: true
+      }
+    });
+    const inner = (
+      <Box
+        bgcolor="background.paper"
+        m={1}
+        border={1}
+        style={{ width: "5rem", height: "5rem" }}
+      />
+    );
     const { classes } = this.props;
     const { imgUrl } = this.state;
+
     return (
       <Grid container direction="row" justify="center" alignItems="center">
         <div>
-        <Box color="white" css={{ bgcolor: 'palevioletred', p: 1, textTransform: 'uppercase' }}>
-  Home Screen
-</Box>
+          <ThemeProvider theme={theme}>
+            <Box
+              color="primary.main"
+              // bgcolor="background.paper"
+              fontFamily="h1.fontFamily"
+              fontSize={{
+                xs: "h6.fontSize",
+                sm: "h4.fontSize",
+                md: "h3.fontSize"
+              }}
+              p={{ xs: 2, sm: 3, md: 4 }}
+            >
+              <h1>Home Screen</h1>
+            </Box>
+          </ThemeProvider>
 
           <Dropzone onDrop={acceptedFiles => this.handleChange(acceptedFiles)}>
             {({ getRootProps, getInputProps }) => (
