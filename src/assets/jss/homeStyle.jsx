585104c8a2bff4dafd477940ff48a2762540a5d6
justunee <lifeistogive@gmail.com>
2019-04-21 00:27:17 +0700

    Button beauty

--- src/assets/jss/homeStyle.jsx
+++ src/assets/jss/homeStyle.jsx
@@ -1,4 +1,4 @@
-const homeStyle = {
+const homeStyle = theme => ({
   dropZone: {
     flex: 1,
     display: "flex",
@@ -13,6 +13,77 @@ const homeStyle = {
     color: "#bdbdbd",
     outline: "none",
     transition: "border .24s ease-in-out"
+  },
+  root: {
+    display: "flex",
+    flexWrap: "wrap",
+    minWidth: 300,
+    width: "100%"
+  },
+  image: {
+    position: "relative",
+    height: 200,
+    [theme.breakpoints.down("xs")]: {
+      width: "100% !important", // Overrides inline-style
+      height: 100
+    },
+    "&:hover, &$focusVisible": {
+      zIndex: 1,
+      "& $imageBackdrop": {
+        opacity: 0.15
+      },
+      "& $imageMarked": {
+        opacity: 0
+      },
+      "& $imageTitle": {
+        border: "4px solid currentColor"
+      }
+    }
+  },
+  focusVisible: {},
+  imageButton: {
+    position: "absolute",
+    left: 0,
+    right: 0,
+    top: 0,
+    bottom: 0,
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    color: theme.palette.common.white
+  },
+  imageSrc: {
+    position: "absolute",
+    left: 0,
+    right: 0,
+    top: 0,
+    bottom: 0,
+    backgroundSize: "cover",
+    backgroundPosition: "center 40%"
+  },
+  imageBackdrop: {
+    position: "absolute",
+    left: 0,
+    right: 0,
+    top: 0,
+    bottom: 0,
+    backgroundColor: theme.palette.common.black,
+    opacity: 0.4,
+    transition: theme.transitions.create("opacity")
+  },
+  imageTitle: {
+    position: "relative",
+    padding: `${theme.spacing.unit * 2}px ${theme.spacing.unit * 4}px ${theme
+      .spacing.unit + 6}px`
+  },
+  imageMarked: {
+    height: 3,
+    width: 18,
+    backgroundColor: theme.palette.common.white,
+    position: "absolute",
+    bottom: -2,
+    left: "calc(50% - 9px)",
+    transition: theme.transitions.create("opacity")
   }
   /**
  *   flexDirection: "column"
@@ -24,6 +95,6 @@ const homeStyle = {
     outline: none,
     transition: border .24s ease-in-out,
  */
-};
+});
 
 export default homeStyle;
